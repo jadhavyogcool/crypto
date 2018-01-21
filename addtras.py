39a7da57deb300def988f053706f546618d52ed5
yogesh jadhav <jadhavyogcool@gmail.com>
2018-01-20 18:02:52 -0800

    Add files via upload

---
+++ addtras.py
@@ -0,0 +1,20 @@
+class Blockchain(object):
+    ...
+    
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
